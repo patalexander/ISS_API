2e0c0f3a466cfdcdf132ea22b03c7dd8ae470fe3
Patricia Alexander <65757094+patalexander@users.noreply.github.com>
2021-06-15 13:02:29 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+
+# use the url with the json key value pairs
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+
+data = response.json()
+
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+
+iss_position = (latitude, longitude)
+
+print(iss_position)
